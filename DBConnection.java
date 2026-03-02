04494c7f4d37f7a12801a03adf1f041e2517c407
Irumipramisha <irumi2002@gmail.com>
2026-03-02 23:57:01 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,27 @@
+package backend; 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/personal_finance_tracker";
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "enter your password here"; // your password
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            System.out.println("❌ Connection Failed!");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        if (getConnection() != null) {
+            System.out.println("✅ Successfully Connected to Database!");
+        }
+    }
+} 
